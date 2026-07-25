fac9848619ca70b656a40d1860cbeb811dcb394c
farhan-farabi23 <rahman.farabi01@yahoo.com>
2026-07-26 02:01:25 +0300

    lec2 init

---
+++ lec2.js
@@ -0,0 +1,52 @@
+console.log("Lecture 2 : Practice");
+
+//single line comment
+/* This is a
+multi line comment */
+
+//Arithmetic Operators
+let a = 5;
+let b = 2;
+console.log("a+b = ",a + b); //Addition
+console.log("a-b = ",a - b); //Subtraction
+console.log("a*b = ",a * b); //Multiplication
+console.log("a/b = ",a / b); //Division
+console.log("a%b = ",a % b); //Modulus
+console.log("a**b = ",a ** b); //Exponentiation
+
+//Unary Operators
+//Post-Increment and Post-Decrement
+a++,
+console.log("a++ = ",a);
+b--;
+console.log("b-- = ",b); 
+
+//Pre-Increment and Pre-Decrement
+++a;
+console.log("++a = ",a);
+--b;
+console.log("--b = ",b);
+
+//Assignment Operators
+let c = 10;
+console.log("c = ",c); //Assignment
+c += 5;
+console.log("c += 5 = ",c); //Addition Assignment
+c -= 3;
+console.log("c -= 3 = ",c); //Subtraction Assignment
+c *= 2;
+console.log("c *= 2 = ",c); //Multiplication Assignment
+c /= 4;
+console.log("c /= 4 = ",c); //Division Assignment
+c %= 3;
+console.log("c %= 3 = ",c); //Modulus Assignment
+
+//Comparison Operators
+let d = 10;
+let e = 20;
+console.log("d == e : ",d == e);
+console.log("d != e : ",d != e);
+console.log("d < e : ",d < e);
+console.log("d > e : ",d > e);
+console.log("d <= e : ",d <= e);
+console.log("d >= e : ",d >= e);
