58281f071a6cdf1f766e7970e36916b94a597a1d
farhan-farabi23 <rahman.farabi01@yahoo.com>
2026-07-25 20:40:36 +0300

    Lecture 1 done

--- lec1.js
+++ lec1.js
@@ -1,1 +1,46 @@
-console.log("Hello, World!");
+let age = 25;
+console.log(age);
+
+let name = "John Doe";
+console.log(name);
+
+let isFollow = true;
+console.log(isFollow);
+
+let x = undefined;
+console.log(x);
+
+let y = BigInt(9007199254740991);
+console.log(y);
+
+const student = {
+    name : "Farhan",
+    age : 20,
+    marks : 9.5,
+    isPass : true,
+};
+
+student["age"] = student["age"] + 1;
+
+console.log(student);
+
+
+//Lecture 1 : Practice
+
+const product = {
+    name : "Parker Jotter Standerd CT Ball Pen (Black)",
+    numberReview : 7002,
+    offer : 5,
+    price : 270,
+};
+console.log(product);
+
+const profile = {
+    userName : "Farhan",
+    isFollow : true,
+    followers : 569,
+    following : 4,
+};
+console.log(profile);
+console.log(typeof profile.isFollow);
+
